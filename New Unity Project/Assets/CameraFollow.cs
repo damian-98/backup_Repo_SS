87b44f20d0f2dc22021c31510c30412be6dfee9a
Damian Tomlinson <dam982010@yahoo.com>
2021-02-16 13:58:15 -0500

    Initial commit

---
+++ New Unity Project/Assets/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿/*  #
+    # 1: Title: "2D Camera Follow in Unity | 2D Game Dev Tutorial" | Author: Muddy Wolf Games | Source: https://www.youtube.com/watch?v=kAlo_pGF178&list=WL&index=48 | Date retrieved: 2/4/2021 @ 1:00 pm
+    #
+*/
+
+// #1: ####################################################################################################################################################
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   public Transform target; // Variable target will follow the target which is the Player. 
+   public Vector3 cameraOffset; // cameraOffset variable is used to keep the camera in postion where it dosent leave the Player.
+   public float followSpeed = 10f; // This followSpeed variable will determine how fast the camera will follow the target which is the Player.
+   public float xMin = 0f; // This xMin variable which is initialized to 0f just sets the camera to postion 0.
+   Vector3 velocity = Vector3.zero; // This piece of code just sits as a refrence when we use Vector3.SmoothDamp.
+
+   void FixedUpdate(){
+       Vector3 targetPos = target.position + cameraOffset; // This line of code just makes the camera follow the Player.
+       Vector3 clampPos = new Vector3(Mathf.Clamp(targetPos.x, xMin, float.MaxValue), targetPos.y, targetPos.z); // This line of code justs controlles how the camera will follow the target.
+       Vector3 smoothPos = Vector3.SmoothDamp(transform.position, clampPos, ref velocity, followSpeed * Time.fixedDeltaTime); // This line of code just takes the current postion of the target and sets how fast the camera will follow behind the target.
+       
+       transform.position = smoothPos;
+   } 
+}
+// ####################################################################################################################################################
---
+++ New Unity Project/Assets/HealthBar.cs
@@ -0,0 +1,34 @@
+﻿/*
+# 
+# 1: Title: "2D Easy Health Bars in Unity / 2021" | Author: Distorted Pixel Studios | Source: https://www.youtube.com/watch?v=v1UGTTeQzbo&list=WL&index=32&t=181s | Date retrieved: 2/12/2021 @ 11:15am
+#
+*/
+
+// #1: ####################################################################################################################################################
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider Slider;
+    public Color Low;//This displays the color bar for low health.
+    public Color High;//This displays the color for high health.
+    public Vector3 offset;// This helps for charaters that dont share the same height.
+
+    public void SetHealth(float health, float maxHealth)
+    {
+        Slider.gameObject.SetActive(health < maxHealth);//This line of code makes it where if the enemy is lower than full health then the health bar will show.
+        Slider.value = health;//This sets the current health.
+        Slider.maxValue = maxHealth;//This sets the max health.
+
+        Slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(Low, High, Slider.normalizedValue);//This line of code sets the color of the health bar depending on how hard the enemy is.
+    }
+
+    void Start()
+    {
+        Slider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + offset);//This code transforms from a 3D world postion to a 2D world postion. It's helps with movement.
+    }
+}
+// ####################################################################################################################################################
