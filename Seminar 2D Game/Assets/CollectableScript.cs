a67ade1e09178d3a53c2d0df12f83a3cdfc03d37
Ryan usher <ryanuhser4@gmail.com>
2021-02-22 00:14:53 -0500

    I added a potion to the scene and I also added player damage by enemies and player health regeneration from postions.

---
+++ Seminar 2D Game/Assets/CollectableScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Potions"))
+        {
+            Heart.numOfHearts += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+}
