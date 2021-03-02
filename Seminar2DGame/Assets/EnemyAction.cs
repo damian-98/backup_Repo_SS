cdb7a6569600a4775c17c78cfd5a7c78ed17b7a5
Damian Tomlinson <dam982010@yahoo.com>
2021-03-02 00:09:25 -0500

    I add a "Game Over" text that pops up in the middle of the screen when player hearts is all gone I added some extra code in the EnemyAction.cs script to make this work.

--- Seminar2DGame/Assets/EnemyAction.cs
+++ Seminar2DGame/Assets/EnemyAction.cs
@@ -4,6 +4,8 @@
 #
 # 2: Title: "HOW TO MAKE 2D MELEE COMBAT - EASY UNITY TUTORIAL" | Author: Blackthornprod | Source: https://www.youtube.com/watch?v=1QfxdUpVh5I&list=WL&index=85 | Date retrieved: 2/16/2021 @ 12:00 pm
 #
+# 3: Title: "Easy HEALTH and SHIELD guide for UNITY | Unity Tutorial" | Author: KeySmash Studios | Source: https://www.youtube.com/watch?v=gLRupxv8AAw | Date retrieved: 3/1/2021 @ 11:45 am
+#
 */
 
 // #1: ####################################################################################################################################################
@@ -17,12 +19,15 @@ public class EnemyAction : MonoBehaviour
    public int MaxHitpoints = 5;//This sets the max hit points.
    public HealthBar Healthbar;// This is referencing my HealthBar script.
 
+   public GameObject gameOver;// This gameOver object will be used to hold the Game Over text.
+
   
 
     void Start()
     {
         Hitpoints = MaxHitpoints;// This means what every number is set for hit points is also the same for max hit points.
         Healthbar.SetMaxHealth(Hitpoints, MaxHitpoints);
+        gameOver.SetActive(false);// SetActive false means that the Game Over text will not pop up as soon as the game starts.
     }
 // #2: ####################################################################################################################################################
    public void TakeDamage(int damage){
@@ -30,7 +35,7 @@ public class EnemyAction : MonoBehaviour
        Healthbar.SetMaxHealth(Hitpoints, MaxHitpoints);
  
        if(Hitpoints <= 0){// This if statement is saying once the enemy gets hit to many time it will be disapear and get destoryed.
-            FindObjectOfType<AudioManager>().Play("EnemyDeath");
+            FindObjectOfType<AudioManager>().Play("EnemyDeath");// The EnemyDeath sound will be played from this object.
 
            Destroy(gameObject);
         }
@@ -40,9 +45,21 @@ public class EnemyAction : MonoBehaviour
     // #2: ####################################################################################################################################################
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        FindObjectOfType<AudioManager>().Play("PlayerHurt");
+        FindObjectOfType<AudioManager>().Play("PlayerHurt");// The PlayerHurt sound will be played from this object.
         Heart.numOfHearts -= 1;
-       
+
+    // #3: ####################################################################################################################################################
+
+        if(Heart.numOfHearts <= 0) // This if block is saying if the num of hearts reach 0 then 
+                                    //the game will be over
+        {
+            Time.timeScale = 0; // Time.timeScale just means how much time has elapsed since a 
+                                // certain moment.
+                                
+            gameOver.SetActive(true);//SetActive true just enables the Game Over text.
+        }
+     // #3: ####################################################################################################################################################
+
     }
 }
 // 1:####################################################################################################################################################
