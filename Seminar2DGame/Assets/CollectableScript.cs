4a613accf73f36accafe7a345a3ae1936cffafe2
Ryan usher <ryanuhser4@gmail.com>
2021-03-04 01:29:35 -0500

    added some comments to my code to make it a bit easier for people to understand how it works.

--- Seminar2DGame/Assets/CollectableScript.cs
+++ Seminar2DGame/Assets/CollectableScript.cs
@@ -6,21 +6,21 @@ public class CollectableScript : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Heart.numOfHearts < 3)
+        if (Heart.numOfHearts < 3)//if statement that checks to see if heart isnt full and if it isnt it adds a heart
         {
-            if (collision.CompareTag("Potions"))
+            if (collision.CompareTag("Potions"))//check to see if the player collider box collided with the potion tag.
             {
-                FindObjectOfType<AudioManager>().Play("Potions");
-                Heart.numOfHearts += 1;
+                FindObjectOfType<AudioManager>().Play("Potions"); 
+                Heart.numOfHearts += 1; //calls the Heartnumber from the Heart script and increments by 1
                 Destroy(collision.gameObject);
             }
         }
-        else if(Heart.numOfHearts == 3)
+        else if(Heart.numOfHearts == 3)//if statement that checks to see if heart is full and if it is instead of adding more it increase coin counter
         {
-            if (collision.CompareTag("Potions"))
+            if (collision.CompareTag("Potions"))//check to see if the player collider box collided with the potion tag.
             {
-                FindObjectOfType<AudioManager>().Play("Potions");
-                CoinCounter.coinAmount += 1;
+                FindObjectOfType<AudioManager>().Play("Potions");//play potion sounds
+                CoinCounter.coinAmount += 1;//calls coinAmount element from the CoinCounter script.
                 Destroy(collision.gameObject);
             }
         }
