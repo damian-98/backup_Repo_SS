9bbdb288adb8c8817dec70a40e5c92274ce83464
Ryan usher <ryanuhser4@gmail.com>
2021-03-07 18:25:56 -0500

    I began implementing the timer of the game, so far it only prints on debug but will continue to work on it so it works in game.

---
+++ Seminar2DGame/Assets/Scripts/TImeCounter.cs
@@ -0,0 +1,23 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TImeCounter : MonoBehaviour
+{
+
+    float currentTime;
+    float startingTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime; //makes it so when game starts, the current time is set to starting time.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime; // the decrement of current time updates evrery frame. however we do not want that,
+                                           // so we use Time.deltaTime to update evry second.
+    }
+}
