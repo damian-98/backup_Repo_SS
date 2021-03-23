cbd6fd11cff97de1b768e905cde221645514af3c
James M <james3397@gmail.com>
2021-03-23 01:07:48 -0400

    added quit scene to build index,and added hover & click sounds to main menu buttons

---
+++ Seminar2DGame/Assets/Scripts/buttonFx.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFx : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+    public void HoverSound(){
+        myFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound(){
+        myFx.PlayOneShot(clickFx);
+    }
+}
