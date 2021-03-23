4a43c8bcaa58a3538182d8f86e36d65340233428
Damian Tomlinson <dam982010@yahoo.com>
2021-03-23 17:33:18 -0400

    I tweaked Ryan's FallRespawn.cs script so that once player loses all hearts from falling it will be game over

--- Seminar2DGame/Assets/Scripts/FallRespawn.cs
+++ Seminar2DGame/Assets/Scripts/FallRespawn.cs
@@ -6,12 +6,25 @@ public class FallRespawn : MonoBehaviour
 {
     [SerializeField] Transform spawnPoint;
 
+    public GameObject gameOver;// This gameOver object will be used to hold the Game Over text.
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             collision.transform.position = spawnPoint.position;
             Heart.numOfHearts -= 1;
+    
+            if(Heart.numOfHearts <= 0) // This if block is saying if the num of hearts reach 0 then 
+                                    //the game will be over
+            {
+             Time.timeScale = 0; // Time.timeScale just means how much time has elapsed since a 
+                                // certain moment.
+
+            FindObjectOfType<AudioManager>().Play("GameOver");
+
+            gameOver.SetActive(true);//SetActive true just enables the Game Over text.
+            }
         }
     }
 }
