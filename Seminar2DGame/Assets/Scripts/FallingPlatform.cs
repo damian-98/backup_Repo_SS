02a8b658bd7f172218436aaa959ed4e200ce3a2c
Ryan usher <ryanuhser4@gmail.com>
2021-04-05 15:18:40 -0400

    I added respawnable platforms to the project.

--- Seminar2DGame/Assets/Scripts/FallingPlatform.cs
+++ Seminar2DGame/Assets/Scripts/FallingPlatform.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class FallingPlatform : MonoBehaviour
 {
     Rigidbody2D rb;
+    Vector2 InitialPos;
+    bool ismovingback;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D> ();
+        InitialPos = transform.position;
+    }
+
+     void Update()
+    {
+        if (ismovingback)
+            transform.position = Vector2.MoveTowards(transform.position, InitialPos, 20f * Time.deltaTime);
+
+        if (transform.position.y == InitialPos.y)
+            ismovingback = false;
     }
 
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name.Equals("Player"))
+        if (collision.gameObject.name.Equals("Player") && !ismovingback)
         {
             Invoke("DropPlatform", 0.5f);
-            Destroy(gameObject, 8f);
         }
     }
 
     void DropPlatform()
     {
         rb.isKinematic = false;
+        Invoke("GetPlatform", 4f);
+    }
+
+    void GetPlatform()
+    {
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+        ismovingback = true;
     }
 }
