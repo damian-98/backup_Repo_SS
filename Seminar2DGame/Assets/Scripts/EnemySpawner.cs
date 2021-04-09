1707b4f4c6f67bcd5238f8716c94227220178444
Damian Tomlinson <dam982010@yahoo.com>
2021-04-09 19:37:26 -0400

    I fixed the issue where you can only see resawnable enemies through scene view and not game view

--- Seminar2DGame/Assets/Scripts/EnemySpawner.cs
+++ Seminar2DGame/Assets/Scripts/EnemySpawner.cs
@@ -2,22 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
-{
-    public GameObject enemy;
-    float randx;
-    Vector2 whereToSpawn;
-    public float spawnRate = 2f;
-    float nextSpawn = 0.0f;
+public class EnemySpawner : MonoBehaviour {
 
-    void Update()
-    {
-       if(Time.time > nextSpawn)
-       {
-           nextSpawn = Time.time + spawnRate;
-           randx = Random.Range(-8.4f, 8.4f);
-           whereToSpawn = new Vector2(randx, transform.position.y);
-           Instantiate(enemy, whereToSpawn, Quaternion.identity);
-       } 
-    }
+	public Transform[] spawnPoints;
+	public GameObject[] enemyPrefabs;
+    int randomSpawn, randomEnemy;
+    public static bool spawnAllowed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawnAllowed = true;
+        InvokeRepeating("SpawnAEnemy", 0f, 15f);
+	}
+
+	void SpawnAEnemy()
+	{
+		if (spawnAllowed) {
+			randomEnemy = Random.Range(0, enemyPrefabs.Length);
+			randomSpawn = Random.Range(0, spawnPoints.Length);
+
+			Instantiate(enemyPrefabs[randomEnemy], spawnPoints[randomSpawn].position, Quaternion.identity);
+		}
+	}
 }
