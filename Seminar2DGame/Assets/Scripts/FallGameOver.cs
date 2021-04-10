b6f29a0e5a718a757964445f6a82711b07cdf826
Damian Tomlinson <dam982010@yahoo.com>
2021-04-10 03:19:25 -0400

    I fixed the fall detection collider because it seemed since I made the map bigger not knowing I had to adjust the detection collider to the same length as map so now player will respawn again

---
+++ Seminar2DGame/Assets/Scripts/FallGameOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallGameOver : MonoBehaviour
+{
+     [SerializeField] Transform spawnPoint;
+    public GameObject gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Awake()
+    {
+         Heart.numOfHearts -= 1;
+            
+             FindObjectOfType<AudioManager>().Play("PlayerHurt");
+        if(Heart.numOfHearts == 0)
+        {
+                FindObjectOfType<AudioManager>().Play("GameOver");
+           
+
+
+            gameOver.SetActive(true);//SetActive true just enables the Game Over text.
+        }
+    }
+ 
+        
+    
+}
