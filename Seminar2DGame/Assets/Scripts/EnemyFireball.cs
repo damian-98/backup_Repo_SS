53d4bcdcefc2877ea6a307431ce72cb32cebe35f
Damian Tomlinson <dam982010@yahoo.com>
2021-04-13 01:24:41 -0400

    I edit the EnemyFireball.cs script so that now the enemy Projectiles can damage the player health

--- Seminar2DGame/Assets/Scripts/EnemyFireball.cs
+++ Seminar2DGame/Assets/Scripts/EnemyFireball.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class EnemyFireball : MonoBehaviour {
+public class EnemyFireball : MonoBehaviour 
+{
 
-	public float speed;
-
+	public HealthBar Healthbar;// This is referencing my HealthBar script.
 	private Transform Player;
+	public float speed;
 	private Vector2 target;
+	public Animator animator;
+	public GameObject gameOver;// This gameOver object will be used to hold the Game Over text.
 
 	void Start()
 	{
 		Player = GameObject.FindGameObjectWithTag("Player").transform;
-
 		target = new Vector2(Player.position.x, Player.position.y);
+		gameOver.SetActive(false);// SetActive false means that the Game Over text will not pop up as soon as the game starts.
 	}
 
 	void Update()
 	{
 		transform.position = Vector2.MoveTowards(transform.position,target, speed * Time.deltaTime);
-		
+
 		if(transform.position.x == target.x && transform.position.y == target.y)
 		{
 			DestroyProjectile();
@@ -31,6 +35,22 @@ public class EnemyFireball : MonoBehaviour {
 		if(other.CompareTag("Player"))
 		{
 			DestroyProjectile();
+			FindObjectOfType<AudioManager>().Play("PlayerHurt"); // The PlayerHurt sound will be played from this object.
+
+        	Heart.numOfHearts -= 1;
+
+        	if(Heart.numOfHearts <= 0) 	// This if block is saying if the num of hearts reach 0 then 
+                                   		//the game will be over
+        	{
+
+            Time.timeScale = 0;
+
+            FindObjectOfType<AudioManager>().Play("PlayerDeath");
+			FindObjectOfType<AudioManager>().Play("GameOver");
+			gameOver.SetActive(true);//SetActive true just enables the Game Over text.
+            animator.SetBool("Dead", true);
+
+        	}
 		}
 	}
 
