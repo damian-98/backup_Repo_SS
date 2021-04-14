c86f9c267ae5ae334d2f20b7fb2381cea8f646a0
Damian Tomlinson <dam982010@yahoo.com>
2021-04-14 02:31:48 -0400

    I made comments to my scripts that I made

--- Seminar2DGame/Assets/Scripts/EnemyFireball.cs
+++ Seminar2DGame/Assets/Scripts/EnemyFireball.cs
@@ -1,30 +1,40 @@
-﻿using System.Collections;
+﻿/*
+# 
+# 1: Title: "SHOOTING/FOLLOW/RETREAT ENEMY AI WITH UNITY AND C# - EASY TUTORIAL" | Author: Blackthornprod| Source: https://www.youtube.com/watch?v=_Z1t7MNk0c4&list=WL&index=187 | Date retrieved: 4/12/2021 @ 3:05 pm
+#
+*/
+
+// #1: ######################################################################################################
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //Using Unity's UI library to use the gameOver text
 
 public class EnemyFireball : MonoBehaviour 
 {
 
-	public HealthBar Healthbar;// This is referencing my HealthBar script.
-	private Transform Player;
-	public float speed;
+	private HealthBar Healthbar;// This is referencing my HealthBar script to  access player health.
+	private Transform Player; 
+	public float speed; // Variable to control the speed of projectile
 	private Vector2 target;
-	public Animator animator;
 	public GameObject gameOver;// This gameOver object will be used to hold the Game Over text.
 
 	void Start()
 	{
-		Player = GameObject.FindGameObjectWithTag("Player").transform;
-		target = new Vector2(Player.position.x, Player.position.y);
+		Player = GameObject.FindGameObjectWithTag("Player").transform; // This line of code is going to just find the gameObject with tag Player.
+		target = new Vector2(Player.position.x, Player.position.y);// This line of code is going to find the gameObject with tag Player position of its x&y axis.
 		gameOver.SetActive(false);// SetActive false means that the Game Over text will not pop up as soon as the game starts.
 	}
 
 	void Update()
 	{
-		transform.position = Vector2.MoveTowards(transform.position,target, speed * Time.deltaTime);
+		transform.position = Vector2.MoveTowards(transform.position,target, speed * Time.deltaTime);// This line of code is going to make the projectile move towards
+																									// gameObject tag "Player" position.
 
-		if(transform.position.x == target.x && transform.position.y == target.y)
+		if(transform.position.x == target.x && transform.position.y == target.y) // This if block is saying if the projectile is equal to the same postion of
+																				// gameObject tag Player then when projectile reaches player postion
+																				// the projectile will get destoryed.
+																				
 		{
 			DestroyProjectile();
 		}
@@ -32,9 +42,11 @@ public class EnemyFireball : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if(other.CompareTag("Player"))
+		if(other.CompareTag("Player"))// This if statment is saying if the gameObject is tag Player and projectile hits tag Player
+										// it will get destoryed on contact and play playerhurt sound. Also will decrement a heart
+										// sprite from tag Player health.
 		{
-			DestroyProjectile();
+			Destroy(gameObject);
 			FindObjectOfType<AudioManager>().Play("PlayerHurt"); // The PlayerHurt sound will be played from this object.
 
         	Heart.numOfHearts -= 1;
@@ -43,19 +55,21 @@ public class EnemyFireball : MonoBehaviour
                                    		//the game will be over
         	{
 
-            Time.timeScale = 0;
+            Time.timeScale = 0; // This line of code just freezes the game.
 
             FindObjectOfType<AudioManager>().Play("PlayerDeath");
 			FindObjectOfType<AudioManager>().Play("GameOver");
 			gameOver.SetActive(true);//SetActive true just enables the Game Over text.
-            animator.SetBool("Dead", true);
 
         	}
 		}
 	}
 
-	void DestroyProjectile()
+	void DestroyProjectile() // inside this function is a Destroy gameObject method that 
+							// I created in the update method to destroy the projectile
+							// when it finds target gameObject tag Player position.
 	{
 		Destroy(gameObject);
 	}
 }
+// #1: ######################################################################################################
--- Seminar2DGame/Assets/Scripts/Monster.cs
+++ Seminar2DGame/Assets/Scripts/Monster.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿/*
+# 
+# 1: Title: "SHOOTING/FOLLOW/RETREAT ENEMY AI WITH UNITY AND C# - EASY TUTORIAL" | Author: Blackthornprod| Source: https://www.youtube.com/watch?v=_Z1t7MNk0c4&list=WL&index=187 | Date retrieved: 4/12/2021 @ 3:05 pm
+#
+*/
+
+// #1: ######################################################################################################
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Monster : MonoBehaviour {
 
-private float timeBtwShots;
-public float startTimeBtwShots;	
+private float timeBtwShots; // This variable will be used to set how fast enemies will fire projectiles at a time.
+public float startTimeBtwShots;	// This variable will be used to control how often enemies will fire.
 
-public GameObject projectile;
+public GameObject projectile; // This variable will be used to reference the gameObject tag projectile in Unity.
 
 void Start()
 {
@@ -16,15 +23,18 @@ void Start()
 
 void Update()
 {
-	if (timeBtwShots <= 0)
+	if (timeBtwShots <= 0) // This if statement is saying if timeBtwShots less or equal to 0 then it will spawn a projectile
+							// from the enemys position with no rotation
 	{
 		Instantiate(projectile, transform.position, Quaternion.identity);
-		timeBtwShots = startTimeBtwShots;
+		timeBtwShots = startTimeBtwShots; // This line of code will prevent the enemy from firing projectiles every frame.
 		
 	}else{
 
-		timeBtwShots -= Time.deltaTime;
+		timeBtwShots -= Time.deltaTime; //this line of code will just decrement how ever long enemies timeBtwShots is back to 0.
+		
 
 		}
 	}
 }
+// #1: ######################################################################################################
