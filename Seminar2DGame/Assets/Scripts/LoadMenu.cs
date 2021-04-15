f4fe1ca038754d9345ef524f08abe2774ed71011
James M <james3397@gmail.com>
2021-04-15 18:05:56 -0400

    Fixed Level Button in Main Menu, Now have an option of selecting what level you want to go to

---
+++ Seminar2DGame/Assets/Scripts/LoadMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour
+{
+   public void LevelOne()
+   {
+       SceneManager.LoadScene("IntroductoryLevelScene");
+   }
+   public void LevelTwo()
+   {
+       SceneManager.LoadScene("LevelTwo");
+   }
+}
