ecca4b3198c66eb3ae0082e607b8fdff4d70543b
Damian Tomlinson <dam982010@yahoo.com>
2021-04-17 18:10:17 -0400

    I added a fireball pick up sound for the fireballs

---
+++ Seminar2DGame/Assets/Scripts/FireballPickupSound.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballPickupSound : MonoBehaviour
+{
+     private void OnTriggerEnter2D(Collider2D col)
+    {
+        FindObjectOfType<AudioManager>().Play("FireBallPickUp");
+    }
+}
