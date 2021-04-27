e6cb616c0488c83948cbae88ec7a2f6f644a811b
Ryan usher <ryanuhser4@gmail.com>
2021-04-27 16:18:32 -0400

    addedback new scripts, see if it works.

---
+++ Seminar2DGame/Assets/Scripts/BAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAttack : MonoBehaviour
+{
+	public Vector3 attackOffset;
+	public float attackRange = 1f;
+	public GameObject gameover;
+	public Animator animator;
+
+	public void Attack()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x;
+		pos += transform.up * attackOffset.y;
+
+		Heart.numOfHearts -= 1;
+		Over();
+	}
+
+	public void EnragedAttack()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x;
+		pos += transform.up * attackOffset.y;
+
+		Heart.numOfHearts -= 1;
+	}
+
+	public void Over()
+	{
+		if (Heart.numOfHearts == 0)
+		{
+			StartCoroutine("GameOver");
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Vector3 pos = transform.position;
+		pos += transform.right * attackOffset.x;
+		pos += transform.up * attackOffset.y;
+
+		Gizmos.DrawWireSphere(pos, attackRange);
+	}
+
+	IEnumerator GameOver()
+	{
+		FindObjectOfType<AudioManager>().Play("PlayerDeath");
+		FindObjectOfType<AudioManager>().Play("GameOver");
+		animator.SetBool("Dead", true);
+		yield return new WaitForSeconds(0.8f);
+		gameover.SetActive(true);//SetActive true just enables the Game Over text.
+		Time.timeScale = 0;
+	}
+}
